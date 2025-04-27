6f2ca7267bc779666069d13a7e151c35dbaf6485
Wenhan Hu <134460467+HCO168@users.noreply.github.com>
2025-04-27 12:34:48 -0700

    1.7

--- app/src/main/java/com/yourname/plantgame/helperclass/Random2DArray.java
+++ app/src/main/java/com/yourname/plantgame/helperclass/Random2DArray.java
@@ -1,6 +1,7 @@
 package com.yourname.plantgame.helperclass;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Random2DArray<T>{
@@ -9,13 +10,11 @@ public class Random2DArray<T>{
     ArrayList<T> arrayList=new ArrayList<>();
     public Random2DArray(T[][] array2D){
         this.array2D=array2D;
-        /*
-        this.array2D=array2D.clone();
+        //this.array2D=array2D.clone();
         for(int r=0;r<array2D.length;r++){
             this.array2D[r]=array2D[r].clone();
             totalLength+=array2D[r].length;
         }
-         */
     }
     private static int randomInt(int a,int b){//[a,b)
         return (int) ((b-a)*Math.random())+a;
@@ -34,7 +33,7 @@ public class Random2DArray<T>{
     public T eliminateRandom(){
         if(arrayList.isEmpty()){
             for(int i=0;i<array2D.length;i++){
-                arrayList.addAll(List.of(array2D[i]));
+                arrayList.addAll(Arrays.asList(array2D[i]));
             }
         }
         return arrayList.remove(randomInt(0,arrayList.size()));
